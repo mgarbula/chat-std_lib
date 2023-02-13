a8da8cbe223c62d7bbd3b9c2988ffc168c75b735
mgarbula <mgarbula@student.agh.edu.pl>
2023-02-14 00:19:16 +0100

    client receive messages from others

--- src/client/Client.java
+++ src/client/Client.java
@@ -6,19 +6,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.net.SocketException;
 
 public class Client {
 
     private final String name;
     private final String prefix;
     private Socket socket;
-    private AtomicBoolean exit = new AtomicBoolean(false);
-    private AtomicBoolean changed = new AtomicBoolean(false);
 
     public Client(String name) {
         this.name = name;
-        prefix = name + ": ";
+        this.prefix = name + ": ";
         try {
             socket = new Socket("localhost", 9999);
         } catch (IOException e) {
@@ -27,49 +25,41 @@ public class Client {
         }
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         System.out.println("Podaj swoje imie");
         String name = new BufferedReader(new InputStreamReader(System.in)).readLine();
         Client client = new Client(name);
         client.start();
     }
 
-    public void start() throws InterruptedException, IOException {
+    public void start() throws IOException {
         Receiver receiver = new Receiver();
         Thread receiveThread = new Thread(receiver);
         receiveThread.start();
 
-        Sender sender = new Sender(receiveThread);
-        Thread sendThread = new Thread(sender);
-        sendThread.start();
+        Sender sender = new Sender();
+        sender.run();
 
-        receiveThread.join();
-        System.out.println("KONIEC startu");
         socket.close();
     }
 
-    class Sender extends Thread {
-
-        private Thread receive;
-
-        public Sender(Thread receive) {
-            this.receive = receive;
-        }
+    class Sender {
 
         public void run() {
             try {
                 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
                 BufferedWriter message = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
                 String s = null;
+                message.write(name + "\n");
+                message.flush();
                 while ((s = br.readLine()) != null) {
-                    changed.compareAndSet(false, true);
                     if (s.equals("exit")) {
-                        exit.compareAndSet(false, true);
+                        message.write("Log out" + "\n");
+                        message.flush();
                         break;
                     }
                     message.write(prefix + s + "\n");
                     message.flush();
-                    changed.compareAndSet(true, false);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -80,21 +70,14 @@ public class Client {
     class Receiver extends Thread {
 
         public void run() {
-            System.out.println("Odbieram");
             try {
                 BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 String s = null;
-                while (!changed.get());
-                while (!exit.get() && (s = br.readLine()) != null) {
-                    if (exit.get()) {
-                        break;
-                    }
+                while ((s = br.readLine()) != null) {
                     System.out.println(s);
-                    while (!changed.get());
-                    if (exit.get()) {
-                        break;
-                    }
                 }
+            } catch (SocketException e) {
+                System.out.println("Log out");
             } catch (IOException e) {
                 e.printStackTrace();
             }
