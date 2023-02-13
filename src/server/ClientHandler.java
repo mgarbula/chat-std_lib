9eec3097256a2d9d7c3f0ded74a45c8965e98d72
mgarbula <mgarbula@student.agh.edu.pl>
2023-02-14 00:19:45 +0100

    server can send messages to many clients

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -6,29 +6,57 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
-import java.net.SocketException;
+import java.util.ArrayList;
 
-public class ClientHandler {
+public class ClientHandler implements Runnable {
+
+    public static ArrayList<ClientHandler> handlers = new ArrayList<>();
+    private final BufferedReader incomeMessageStream;
+    private final BufferedWriter outcomeMessageStream;
+    private final String name;
     private final Socket socket;
 
-    public ClientHandler(Socket socket) {
+    public ClientHandler(Socket socket, String name) throws IOException {
         this.socket = socket;
+        this.name = name;
+        this.incomeMessageStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.outcomeMessageStream = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        handlers.add(this);
+        sendHello();
     }
 
-    public void start() {
-        try {
-            BufferedReader incomeMessageStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String incomeMessage = null;
-            BufferedWriter sendMessageStream = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            while ((incomeMessage = incomeMessageStream.readLine()) != null) {
-                System.out.println(incomeMessage);
-                sendMessageStream.write(incomeMessage);
-                sendMessageStream.flush();
+    private void sendHello() throws IOException {
+        for (ClientHandler handler : handlers) {
+            if (!handler.name.equals(name)) {
+                handler.outcomeMessageStream.write(name + " just logged in" + "\n");
+                handler.outcomeMessageStream.flush();
             }
+        }
+    }
+
+    public void run() {
+        String message;
+        try {
+            while ((message = incomeMessageStream.readLine()) != null) {
+                if (message.equals("Log out")) {
+                    System.out.println("You just logged out");
+                    handlers.remove(this);
+                    for (ClientHandler handler : handlers) {
+                        handler.outcomeMessageStream.write(name + " just logged out" + "\n");
+                        handler.outcomeMessageStream.flush();
+                    }
+                    break;
+                }
+                for (ClientHandler handler : handlers) {
+                    if (!handler.name.equals(name)) {
+                        handler.outcomeMessageStream.write(message + "\n");
+                        handler.outcomeMessageStream.flush();
+                    }
+                }
+            }
+            outcomeMessageStream.close();
             incomeMessageStream.close();
-            sendMessageStream.close();
-        } catch (SocketException e) {
-            System.out.println("SERVER: one of clients logged out");
+            socket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,6 +1,8 @@
 package server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,7 +17,9 @@ public class Server {
     public void startServer() throws IOException {
         while (true) {
             Socket socket = serverSocket.accept();
-            AcceptNewClient accept = new AcceptNewClient(socket);
+            System.out.println("SERVER: new client joined");
+            String name = new BufferedReader(new InputStreamReader(socket.getInputStream())).readLine();
+            ClientHandler accept = new ClientHandler(socket, name);
             Thread thread = new Thread(accept);
             thread.start();
         }
